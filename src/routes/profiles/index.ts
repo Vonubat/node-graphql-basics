288e47174a2730933e85b9637d5cb4f75af35587
Tabunov Egor <Vonubat@gmail.com>
2023-01-31 19:22:28 +0300

    fix: profiles according to tests

--- src/routes/profiles/index.ts
+++ src/routes/profiles/index.ts
@@ -2,6 +2,8 @@ import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-sc
 import { idParamSchema } from '../../utils/reusedSchemas';
 import { createProfileBodySchema, changeProfileBodySchema } from './schema';
 import type { ProfileEntity } from '../../utils/DB/entities/DBProfiles';
+import type { UserEntity } from '../../utils/DB/entities/DBUsers';
+import type { MemberTypeEntity } from '../../utils/DB/entities/DBMemberTypes';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
@@ -27,7 +29,7 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
           equals: profileId,
         });
       if (!foundedProfile) {
-        throw fastify.httpErrors.notFound();
+        throw fastify.httpErrors.notFound('Profile not found');
       }
       return foundedProfile;
     }
@@ -42,6 +44,33 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
     },
     async function (request, reply): Promise<ProfileEntity> {
       const { body: profileDTO } = request;
+      const { userId, memberTypeId } = profileDTO;
+
+      const foundedUser: UserEntity | null = await fastify.db.users.findOne({
+        key: 'id',
+        equals: userId,
+      });
+      if (!foundedUser) {
+        throw fastify.httpErrors.badRequest(`User doesn't exist`);
+      }
+
+      const foundedProfile: ProfileEntity | null =
+        await fastify.db.profiles.findOne({
+          key: 'userId',
+          equals: userId,
+        });
+      if (foundedProfile) {
+        throw fastify.httpErrors.badRequest(`User already has a profile`);
+      }
+
+      const foundedMemberType: MemberTypeEntity | null =
+        await fastify.db.memberTypes.findOne({
+          key: 'id',
+          equals: memberTypeId,
+        });
+      if (!foundedMemberType) {
+        throw fastify.httpErrors.badRequest(`MemberTypeId is incorrect`);
+      }
 
       const createdProfile: ProfileEntity = await fastify.db.profiles.create(
         profileDTO
@@ -66,7 +95,7 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
           equals: profileId,
         });
       if (!foundedProfile) {
-        throw fastify.httpErrors.notFound();
+        throw fastify.httpErrors.badRequest(`Profile doesn't exist`);
       }
 
       const deletedProfile: ProfileEntity = await fastify.db.profiles.delete(
@@ -94,7 +123,7 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
           equals: profileId,
         });
       if (!foundedProfile) {
-        throw fastify.httpErrors.notFound();
+        throw fastify.httpErrors.badRequest(`Profile doesn't exist`);
       }
 
       const updatedProfile: ProfileEntity = await fastify.db.profiles.change(
